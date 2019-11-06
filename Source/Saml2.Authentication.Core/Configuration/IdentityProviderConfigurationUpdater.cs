9a849463008c8137303afa1aaa4cbe70777dc0e8
Olivier DAHON <odahon@index-education.fr>
2019-11-06 14:47:07 +0100

    Add log

--- Source/Saml2.Authentication.Core/Configuration/IdentityProviderConfigurationUpdater.cs
+++ Source/Saml2.Authentication.Core/Configuration/IdentityProviderConfigurationUpdater.cs
@@ -1,10 +1,10 @@
 ﻿using dk.nita.saml20;
 using dk.nita.saml20.Schema.Metadata;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Saml2.Authentication.Core.Factories;
 using Saml2.Authentication.Core.Schema.Metadata;
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Net;
@@ -19,18 +19,26 @@ namespace Saml2.Authentication.Core.Configuration
     {
         private readonly Saml2Configuration configuration;
         private readonly ICertificateFactory certificateFactory;
+        private readonly ILogger logger;
         private readonly int delay = 24 * 60 * 60 * 1000; // 24h
 
-        public IdentityProviderConfigurationUpdater(Saml2Configuration configuration, ICertificateFactory certificateFactory)
+        public IdentityProviderConfigurationUpdater(
+            Saml2Configuration configuration, 
+            ICertificateFactory certificateFactory,
+            ILoggerFactory loggerFactory)
         {
             this.configuration = configuration;
             this.certificateFactory = certificateFactory;
+            logger = loggerFactory.CreateLogger(typeof(IdentityProviderConfigurationUpdater));
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
+            logger.LogDebug($"Entering {nameof(ExecuteAsync)}", stoppingToken);
+
             while (!stoppingToken.IsCancellationRequested)
             {
+                logger.LogInformation("Performing periodic update");
                 IList<IdentityProviderConfiguration> configurations = configuration.IdentityProviderConfiguration;
                 foreach (IdentityProviderConfiguration configuration in configurations)
                 {
@@ -48,6 +56,7 @@ namespace Saml2.Authentication.Core.Configuration
 
         private async Task UpdateConfigurationAsync(IdentityProviderConfiguration configuration)
         {
+            logger.LogInformation($"Updating {configuration.FederationMetadata}");
             WebRequest webRequest = WebRequest.Create(configuration.FederationMetadata);
 
             try
@@ -104,7 +113,7 @@ namespace Saml2.Authentication.Core.Configuration
             }
             catch (Exception e)
             {
-
+                logger.LogError(e, $"Error while processing {configuration.Name} {configuration.FederationMetadata}");
             }
         }
     }
